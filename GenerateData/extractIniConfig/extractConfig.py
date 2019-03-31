96dc1e35b4c54330e7b864dbf28eac459f883275
Yuguang Yang <yangyutu123@gmail.com>
2019-03-31 14:29:17 -0400

    update config

--- GenerateData/extractIniConfig/extractConfig.py
+++ GenerateData/extractIniConfig/extractConfig.py
@@ -14,16 +14,16 @@ config = []
 
 
 
-idx = np.where((psi6 > 0.6) & (psi6 < 0.8))
+idx = np.where((psi6 > 0.7) & (psi6 < 0.8))
+idxSelect = np.random.choice(idx[0], 10)
+config = config + xyzSampleAll[idxSelect].tolist()
+
+idx = np.where((psi6 > 0.5) & (psi6 < 0.7))
 idxSelect = np.random.choice(idx[0], 20)
 config = config + xyzSampleAll[idxSelect].tolist()
 
-idx = np.where((psi6 > 0.2) & (psi6 < 0.6))
-idxSelect = np.random.choice(idx[0], 40)
-config = config + xyzSampleAll[idxSelect].tolist()
-
-idx = np.where(psi6 < 0.2)
-idxSelect = np.random.choice(idx[0], 40)
+idx = np.where(psi6 < 0.5)
+idxSelect = np.random.choice(idx[0], 70)
 config = config + xyzSampleAll[idxSelect].tolist()
 
 config = np.array(config)
